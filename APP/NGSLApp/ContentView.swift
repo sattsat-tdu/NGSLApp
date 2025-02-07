1212474cf55acb8edc3069afbb418df246e41fbc
sattsat <syuuatit141offer@gmail.com>
2025-02-07 18:21:27 +0900

    initial commit

---
+++ APP/NGSLApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  NGSLApp
+//  
+//  Created by SATTSAT on 2025/02/07
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ APP/NGSLApp/NGSLAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  NGSLAppApp.swift
+//  NGSLApp
+//  
+//  Created by SATTSAT on 2025/02/07
+//  
+//
+
+import SwiftUI
+
+@main
+struct NGSLAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
